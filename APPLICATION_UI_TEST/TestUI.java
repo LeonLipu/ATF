1c5816f53dfdcac4c9632dea4e803a6f034d9671
LeonLipu <lipusipupara@gmail.com>
2017-07-18 03:34:38 +0530

    server and ui boiler plate done

--- APPLICATION_UI_TEST/TestUI.java
+++ APPLICATION_UI_TEST/TestUI.java
@@ -1,10 +1,16 @@
 package APPLICATION_UI_TEST;
 
+
+//import com.sun.jersey.api.client.Client;
+//import com.sun.jersey.api.client.WebResource;
+
+
 /**
  * Created by brahmanandakar on 18/07/17.
  */
 public class TestUI {
     public static void main(String[] args) {
-        System.out.println("use selenium to test");
+
+
     }
 }
