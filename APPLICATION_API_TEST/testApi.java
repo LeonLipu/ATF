7c9eb8818982210cf9398ebf4d293f6081dc9d69
LeonLipu <lipusipupara@gmail.com>
2017-07-18 03:23:15 +0530

    server and ui boiler plate done

---
+++ APPLICATION_API_TEST/testApi.java
@@ -0,0 +1,10 @@
+package APPLICATION_API_TEST;
+
+/**
+ * Created by brahmanandakar on 18/07/17.
+ */
+public class testApi {
+    public static void main(String[] args) {
+        System.out.println("use jersy client to test");
+    }
+}
---
+++ APPLICATION_UI_TEST/TestUI.java
@@ -0,0 +1,10 @@
+package APPLICATION_UI_TEST;
+
+/**
+ * Created by brahmanandakar on 18/07/17.
+ */
+public class TestUI {
+    public static void main(String[] args) {
+        System.out.println("use selenium to test");
+    }
+}
