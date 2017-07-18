aef9975840599eb8657f9b1ff9a37f00389d3a2c
LeonLipu <lipusipupara@gmail.com>
2017-07-18 19:34:25 +0530

    api test been added

--- APPLICATION_API_TEST/testApi.java
+++ APPLICATION_API_TEST/testApi.java
@@ -1,10 +1,25 @@
 package APPLICATION_API_TEST;
 
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+
+
 /**
  * Created by brahmanandakar on 18/07/17.
  */
 public class testApi {
-    public static void main(String[] args) {
-        System.out.println("use jersy client to test");
+
+    @Test
+    public void test() {
+        Response response = get("http://localhost:8080/students");
+        System.out.println(response.getStatusCode());
+        System.out.print(response.getBody());
+        System.out.println(response.getContentType());
     }
+
+
 }
---
+++ APPLICATION_UI_TEST/Testapitest.java
@@ -0,0 +1,10 @@
+package APPLICATION_UI_TEST;
+
+/**
+ * Created by brkar on 7/18/2017.
+ */
+public class Testapitest {
+
+    public static void main(String[] args) {
+    }
+}
